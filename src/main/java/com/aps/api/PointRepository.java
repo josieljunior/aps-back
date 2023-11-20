153de738053b95736728d922cfb8a9ada0201f5b
Josiel Junior <64495134+josieljunior@users.noreply.github.com>
2023-11-20 20:06:52 -0300

    fix

--- src/main/java/com/aps/api/PointRepository.java
+++ src/main/java/com/aps/api/PointRepository.java
@@ -1,7 +1,10 @@
 package com.aps.api;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
    
 }
